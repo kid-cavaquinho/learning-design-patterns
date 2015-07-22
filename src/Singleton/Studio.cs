4e3fa20e2599c12e705776fd3aa0b4a688ba7702
Joao Antao <joao@mindbus.nl>
2015-07-22 16:25:06 +0200

    Solution and first pattern (Singleton)

---
+++ src/Singleton/Studio.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton
+{
+    internal class Studio
+    {
+        // Keeping thread safety without using locks, there's obviously several ways to implement this, for a further explanation check: http://csharpindepth.com/Articles/General/Singleton.aspx
+        private static Studio instance = new Studio();
+        public static Studio Instance
+        {
+            get { return instance; }
+        }
+
+        static Studio() { }
+
+        private Studio()
+        {
+        }
+
+        public bool IsStudioAvailable(int persons)
+        {
+            // Insert your logic
+            return true;
+        }
+
+    }
+}
