991d0106b6d2617112ce0e7df29e0e4d7328b8ba
João Antão <joao.antao@gmail.com>
2015-07-25 16:01:11 +0100

    Added: StudioLazy to example lazy loading

---
+++ src/Singleton/StudioLazy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class StudioLazy
+    {
+        private static readonly Lazy<StudioLazy> lazy = new Lazy<StudioLazy>(() => new StudioLazy());
+
+        public static StudioLazy Instance { get { return lazy.Value; } }
+
+        private StudioLazy()
+        {
+        }
+
+        public bool IsStudioAvailable(int persons)
+        {
+            // Insert your logic
+            return true;
+        }
+    }
+}
