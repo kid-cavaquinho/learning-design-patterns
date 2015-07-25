b63aaf5b80643be2307deea0766744eadb255339
João Antão <joao.antao@gmail.com>
2015-07-25 16:00:53 +0100

    Added: StudioWithoutLock.cs

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -6,8 +6,8 @@ namespace Singleton
     {
         public void Main(string[] args)
         {
-            Console.WriteLine("Can I use a studio for 1 person? {0}", Studio.Instance.IsStudioAvailable(1) ? "Yes" : "No");
-            Console.WriteLine("Can I use a studio for 10 person? {0}", Studio.Instance.IsStudioAvailable(10) ? "Yes" : "No");
+            Console.WriteLine("Can I use a studio for 1 person? {0}", StudioWithoutLock.Instance.IsStudioAvailable(1) ? "Yes" : "No");
+            Console.WriteLine("Can I use a studio for 10 person? {0}", StudioWithoutLock.Instance.IsStudioAvailable(10) ? "Yes" : "No");
             Console.WriteLine("Press any key");
             Console.Read();
         }
